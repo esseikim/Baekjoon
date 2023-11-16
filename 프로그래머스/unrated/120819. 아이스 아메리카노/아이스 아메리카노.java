e6efe7eaa527b290597aec3f1ee89a1bb2aea3bc
esseikim <100960015+esseikim@users.noreply.github.com>
2023-11-16 19:08:37 +0900

    [level unrated] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int money) {
+        int count = money / 5500;
+        int change = money % 5500;
+
+        int[] answer = {count, change};
+        return answer;
+    }
+}
