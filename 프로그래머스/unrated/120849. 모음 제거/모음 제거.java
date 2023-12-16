1fca9eefc064131a4999574e012750a94866f649
esseikim <100960015+esseikim@users.noreply.github.com>
2023-12-16 21:34:47 +0900

    [level unrated] Title: 모음 제거, Time: 0.04 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -1,7 +1,18 @@
-import java.util.regex.*;
-
 class Solution {
     public String solution(String my_string) {
-        return my_string.replaceAll("[aeiou]", "");
+        StringBuilder result = new StringBuilder();
+
+			  // 주어진 문자열을 순회하면서 소문자 모음인 'a', 'e', 'i', 'o', 'u'를 제거하고 결과 문자열을 반환
+        for (char c : my_string.toCharArray()) {
+            if (!isVowel(c)) {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+
+    private boolean isVowel(char c) { // 해당 문자가 모음인지 확인
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
