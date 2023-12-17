51776846b65a1ba7db2dce7396dea1147cb384a8
esseikim <100960015+esseikim@users.noreply.github.com>
2023-12-17 14:05:02 +0900

    [level unrated] Title: 최댓값 만들기 (1), Time: 2.35 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/unrated/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 class Solution {
     public int solution(int[] numbers) {
-        Arrays.sort(numbers);
-        int length = numbers.length;
-        return numbers[length - 1] * numbers[length - 2];
+        Integer[] sorted = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+        Arrays.sort(sorted, Collections.reverseOrder());
+
+        return sorted[0] * sorted[1];
     }
 }
