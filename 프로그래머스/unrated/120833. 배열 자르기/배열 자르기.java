f4f381458597178c25fb595b045fc9623a9c5c55
esseikim <100960015+esseikim@users.noreply.github.com>
2023-12-17 13:37:10 +0900

    [level unrated] Title: 배열 자르기, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
@@ -1,13 +1,7 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        List<Integer> result = new ArrayList<>();
-        
-        for (int i = num1; i <= num2; i++) {
-            result.add(numbers[i]);
-        }
-        
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
     }
 }
