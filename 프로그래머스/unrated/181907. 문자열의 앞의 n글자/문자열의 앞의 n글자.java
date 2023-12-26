391d3bd0e8ee80ca4e51a1c4ffeec14088a44333
esseikim <100960015+esseikim@users.noreply.github.com>
2023-12-26 22:37:33 +0900

    [level unrated] Title: 문자열의 앞의 n글자, Time: 0.03 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,5 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        return my_string.substring(0, n); // 문자열을 자르는 substring 메서드, my_string의 0번째 위치부터 n번째 위치 직전까지의 문자열을 반환
+    }
+}
