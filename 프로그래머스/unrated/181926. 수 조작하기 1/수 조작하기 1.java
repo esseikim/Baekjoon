dc31744fa6c053e676ee3894f013e7263e656cc5
esseikim <100960015+esseikim@users.noreply.github.com>
2023-12-27 22:06:16 +0900

    [level unrated] Title: 수 조작하기 1, Time: 1.40 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,14 +1,19 @@
 public class Solution {
     public int solution(int n, String control) {
         for (char c : control.toCharArray()) {
-            if (c == 'w') {
-                n = Math.min(n + 1, 100000);
-            } else if (c == 's') {
-                n = Math.max(n - 1, -100000);
-            } else if (c == 'd') {
-                n = Math.min(n + 10, 100000);
-            } else if (c == 'a') {
-                n = Math.max(n - 10, -100000);
+            switch (c) { // n 값이 최소값 -100,000과 최대값 100,000을 벗어나지 않도록 보장
+                case 'w':
+                    n = Math.min(n + 1, 100000); // Math.max(a, b) 함수는 a와 b 중에서 큰 값을 반환 
+                    break;
+                case 's':
+                    n = Math.max(n - 1, -100000);
+                    break;
+                case 'd':
+                    n = Math.min(n + 10, 100000);
+                    break;
+                case 'a':
+                    n = Math.max(n - 10, -100000);
+                    break;
             }
         }
         return n;
