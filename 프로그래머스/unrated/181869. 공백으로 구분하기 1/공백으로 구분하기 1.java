d549c51f34fbf4b5efeb7564413a0398866e0266
esseikim <100960015+esseikim@users.noreply.github.com>
2023-12-30 16:57:16 +0900

    [level unrated] Title: 공백으로 구분하기 1, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] words = my_string.split(" "); // 공백을 기준으로 단어 분리
+
+        return words;
+    }
+}
