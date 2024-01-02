56df24a0dad12a2e829bb81ea3960ee5ae69102b
esseikim <100960015+esseikim@users.noreply.github.com>
2024-01-02 17:26:39 +0900

    [level unrated] Title: 뒤에서 5등까지, Time: 0.38 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -1,12 +1,12 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-    public int[] solution(int[] num_list) {
+    public List<Integer> solution(int[] num_list) { // 반환타입이 리스트일 경우
+        List<Integer> result = new ArrayList<>();
         Arrays.sort(num_list);
-        int[] result = new int[5];
 
-        for (int i = 0; i < 5; i++) {
-            result[i] = num_list[i];
+        for (int i = 0; i < 5; i++) { // 가장 작은 5개의 수를 오름차순으로 추출
+            result.add(num_list[i]);
         }
 
         return result;
