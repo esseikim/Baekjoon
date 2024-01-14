acecff59c9110ca8a41445e738d6185bce97fc18
esseikim <100960015+esseikim@users.noreply.github.com>
2024-01-14 15:03:46 +0900

    [level Lv.2] Title: 진료과별 총 예약 횟수 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/Lv.2/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
+++ 프로그래머스/Lv.2/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
@@ -1,5 +1,6 @@
+-- 2022년 5월에 예약한 환자 수를 진료과 코드 별로 조회하며, 결과는 환자 수를 기준으로 내림차순으로 정렬하고, 환자 수가 같다면 진료과 코드를 기준으로 오름차순으로 정렬
 SELECT MCDP_CD AS "진료과코드", COUNT(*) AS "5월예약건수"
 FROM APPOINTMENT
 WHERE TO_CHAR(APNT_YMD, 'YYYY-MM') = '2022-05'
 GROUP BY MCDP_CD
-ORDER BY "5월예약건수", "진료과코드"; 
+ORDER BY "5월예약건수", "진료과코드";
