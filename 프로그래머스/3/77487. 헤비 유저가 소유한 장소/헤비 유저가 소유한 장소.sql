ae7e37171859e43690410e8a75179868f2f45320
esseikim <100960015+esseikim@users.noreply.github.com>
2024-01-26 20:43:14 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,10 +1,10 @@
-SELECT P.ID, P.NAME, P.HOST_ID
-FROM (
-    SELECT HOST_ID
-    FROM PLACES
-    GROUP BY HOST_ID
-    HAVING COUNT(*) >= 2
-    ) H
-JOIN PLACES P
-ON H.HOST_ID = P.HOST_ID
-ORDER BY ID;
+-- 각 레코드에 대해 서브쿼리를 통해 해당 HOST_ID에 대한 레코드 수를 계산하고, 그 수가 2 이상인 경우에만 반환하는 방식으로 중복된 HOST_ID를 찾는다.
+-- 외부 쿼리에서 각 레코드마다 서브쿼리가 실행되므로, 데이터 양이 많아질수록 성능에 영향을 미칠 수 있다.
+SELECT *
+FROM PLACES A
+WHERE (
+		   SELECT COUNT(*) 
+			 FROM PLACES B
+       WHERE A.HOST_ID = B.HOST_ID
+       ) >= 2
+ORDER BY ID
