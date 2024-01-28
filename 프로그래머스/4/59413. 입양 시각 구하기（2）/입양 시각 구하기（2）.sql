1cbc06510942c3b323a90924a5442bd7212445b2
esseikim <100960015+esseikim@users.noreply.github.com>
2024-01-28 19:35:27 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,16 +1,6 @@
--- -- 가상의 테이블 만들고, 레프트 조인, 포맷형식(파일모드) 맞춰서 조건 잡고, count: 그룹지어서 세야함.. group by 
---  -- *: null값 세고, 컬럼이름인 경우 null값 제외 
--- SELECT d.hour, COUNT(o.animal_id) AS count
---   FROM (SELECT LEVEL - 1 AS hour
---           FROM dual
---        CONNECT BY LEVEL <= 24) d
---   LEFT JOIN animal_outs o
---     ON d.hour = TO_CHAR(datetime, 'FMHH24')
---  GROUP BY d.hour
---  ORDER BY d.hour;
-
-
--- SELECT D.HOUR, COUNT(D.HOUR)
+-- 가상의 테이블 만들고, 레프트 조인, 포맷형식(파일모드) 맞춰서 조건 잡고, count: 그룹 지어서 세어야한다.(group by) 
+-- *: null값 세고, 컬럼이름인 경우 null값 제외 
+-- SELECT D.HOUR, COUNT(D.HOUR) AS "COUNT"
 -- FROM (
 --      SELECT LEVEL -1 AS "HOUR"
 --      FROM DUAL
@@ -20,26 +10,21 @@
 --  ON D.HOUR = TO_CHAR(O.DATETIME, 'FMHH24')
 --  GROUP BY D.HOUR
 --  ORDER BY D.HOUR;
-
-
-SELECT D.HOUR, COUNT(O.ANIMAL_ID)
+ 
+ SELECT 
+    HOURS.HOUR,
+    COUNT(HOUR_COUNT.ANIMAL_ID) AS "COUNT"
 FROM (
-     SELECT LEVEL -1 AS "HOUR"
-     FROM DUAL
-     CONNECT BY LEVEL < 25
-     ) D
- LEFT JOIN ANIMAL_OUTS O
- ON D.HOUR = TO_CHAR(O.DATETIME, 'FMHH24')
- GROUP BY D.HOUR
- ORDER BY D.HOUR;
-
-
-
-
-
-
-
-
-
-
-
+    SELECT 
+        TO_CHAR(DATETIME, 'HH24') AS "HOUR",
+        ANIMAL_ID
+    FROM ANIMAL_OUTS
+) HOUR_COUNT
+RIGHT JOIN (
+    SELECT LEVEL - 1 AS "HOUR"
+    FROM DUAL
+    CONNECT BY LEVEL <= 24
+) HOURS
+ON HOUR_COUNT.HOUR = HOURS.HOUR
+GROUP BY HOURS.HOUR
+ORDER BY HOURS.HOUR;
