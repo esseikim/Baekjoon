922ec4cb868f19477fba73b727116db5fa49e1ef
esseikim <100960015+esseikim@users.noreply.github.com>
2024-02-17 20:02:16 +0900

    [level 0] Title: 가위 바위 보, Time: 0.24 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,19 +1,29 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
-class Solution {
-    public String solution(String rsp) {
+public class Solution {
+    public static String solution(String rsp) {
         StringBuilder result = new StringBuilder();
+        
+        // 이길 수 있는 경우의 문자열을 저장한 Map
+        Map<Character, Character> winMap = new HashMap<>();
+        winMap.put('0', '5'); // 바위(0)이 이기는 경우
+        winMap.put('2', '0'); // 가위(2)가 이기는 경우
+        winMap.put('5', '2'); // 보(5)가 이기는 경우
 
-        for (char c : rsp.toCharArray()) {
-            if (c == '2') {
-                result.append('0'); // 바위(0)는 가위(2)를 이김
-            } else if (c == '0') {
-                result.append('5'); // 보(5)를 바위(0)를 이김
-            } else if (c == '5') {
-                result.append('2'); // 가위(2)는 보(5)를 이김
-            }
+        // 주어진 rsp 문자열을 순회하여 이길 수 있는 경우의 문자열을 만든다.
+        for (int i = 0; i < rsp.length(); i++) {
+            result.append(winMap.get(rsp.charAt(i)));
         }
 
         return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String rsp1 = "2";
+        String rsp2 = "205";
+
+        System.out.println(solution(rsp1)); // 출력: "0"
+        System.out.println(solution(rsp2)); // 출력: "052"
     }
 }
