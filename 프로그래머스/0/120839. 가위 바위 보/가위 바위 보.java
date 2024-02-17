c7a4f83a88ae2f2105b2d13c032f44b447b56b04
esseikim <100960015+esseikim@users.noreply.github.com>
2024-02-17 20:13:18 +0900

    [level 0] Title: 가위 바위 보, Time: 0.23 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -7,9 +7,9 @@ public class Solution {
         
         // 이길 수 있는 경우의 문자열을 저장한 Map
         Map<Character, Character> winMap = new HashMap<>();
-        winMap.put('0', '5'); // 바위(0)이 이기는 경우
-        winMap.put('2', '0'); // 가위(2)가 이기는 경우
-        winMap.put('5', '2'); // 보(5)가 이기는 경우
+        winMap.put('0', '5'); // 바위(0)를 이기는 경우
+        winMap.put('2', '0'); // 가위(2)를 이기는 경우
+        winMap.put('5', '2'); // 보(5)를 이기는 경우
 
         // 주어진 rsp 문자열을 순회하여 이길 수 있는 경우의 문자열을 만든다.
         for (int i = 0; i < rsp.length(); i++) {
