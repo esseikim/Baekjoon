e5455e1ad2e8f8f6bb9464717aa4cc306a180a22
esseikim <100960015+esseikim@users.noreply.github.com>
2024-02-17 14:07:42 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,18 +1,18 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
-    public int[] solution(int[] num_list, int n) {
-        int[] result = new int[num_list.length];
+    public List<Integer> solution(int[] num_list, int n) {
+        List<Integer> result = new ArrayList<>();
         
-        // n 번째 이후의 원소들을 결과 배열에 추가
-        int index = 0;
+        // n 번째 이후의 원소들을 결과 리스트에 추가
         for (int i = n; i < num_list.length; i++) {
-            result[index++] = num_list[i];
+            result.add(num_list[i]);
         }
         
-        // n 번째까지의 원소들을 결과 배열에 추가
+        // n 번째까지의 원소들을 결과 리스트에 추가
         for (int i = 0; i < n; i++) {
-            result[index++] = num_list[i];
+            result.add(num_list[i]);
         }
         
         return result;
@@ -22,9 +22,9 @@ public class Solution {
         Solution sol = new Solution();
         int[] num_list1 = {2, 1, 6};
         int[] num_list2 = {5, 2, 1, 7, 5};
-        int[] result1 = sol.solution(num_list1, 1);
-        System.out.println(Arrays.toString(result1)); // 출력: [1, 6, 2]
-        int[] result2 = sol.solution(num_list2, 3);
-        System.out.println(Arrays.toString(result2)); // 출력: [7, 5, 5, 2, 1]
+        List<Integer> result1 = sol.solution(num_list1, 1);
+        System.out.println(result1); // 출력: [1, 6, 2]
+        List<Integer> result2 = sol.solution(num_list2, 3);
+        System.out.println(result2); // 출력: [7, 5, 5, 2, 1]
     }
 }
