e7d8a9e4b14e04b42797683e1c99e7d7a5845932
esseikim <100960015+esseikim@users.noreply.github.com>
2024-02-20 12:19:13 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.51 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -1,29 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
-public class Solution {
-    public static List<Integer> solution(int[] num_list) {
-        // 정수 배열을 리스트로 변환
-        List<Integer> numList = new ArrayList<>();
-        for (int num : num_list) {
-            numList.add(num);
+class Solution {
+    public int[] solution(int[] num_list) {
+        // 원본 배열을 정렬
+        Arrays.sort(num_list); 
+
+        int[] result = new int[num_list.length - 5]; // 결과 배열의 크기는 num_list.length - 5
+
+        // 가장 작은 5개의 수를 제외한 수를 결과 배열에 담음
+        for (int i = 5; i < num_list.length; i++) {
+            result[i - 5] = num_list[i];
         }
-        
-        // 리스트를 오름차순으로 정렬
-        numList.sort(null);
-        
-        // 가장 작은 5개의 수를 제외한 수들을 담을 리스트 생성
-        List<Integer> resultList = new ArrayList<>(numList.subList(5, numList.size()));
-        
-        // 결과 반환
-        return resultList;
-    }
 
-    public static void main(String[] args) {
-        int[] num_list = {12, 4, 15, 46, 38, 1, 14, 56, 32, 10};
-        List<Integer> result = solution(num_list);
-        
-        // 결과 출력
-        System.out.println(result); // 출력: [15, 32, 38, 46, 56]
+        return result;
     }
 }
